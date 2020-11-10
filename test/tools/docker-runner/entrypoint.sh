d06ebeac714570c9edee983371890003ad2cf388
emadum <eric.adum@mongodb.com>
2020-11-10 16:47:10 -0500

    update

--- test/tools/docker-runner/entrypoint.sh
+++ test/tools/docker-runner/entrypoint.sh
@@ -1,5 +1,5 @@
 echo "Getting ${DRIVER_REPOSITORY}@${DRIVER_REVISION} version ${DRIVER_VERSION}"
-git clone -b v${DRIVER_VERSION} $DRIVER_REPOSITORY driver_src
+git clone --depth 1 -b v${DRIVER_VERSION} $DRIVER_REPOSITORY driver_src
 cd driver_src
 
 # Install desired LTS version of Node
@@ -13,7 +13,7 @@ curl https://raw.githubusercontent.com/creationix/nvm/v0.35.3/install.sh | bash 
 # Run the tests with the provided MONGODB_URI
 if [ $DRIVER_VERSION == "3.3.0" ]; then
   echo "Old version, running special test script"
-  ./node_modules/.bin/mongodb_test_runner -s -l -e single test/core test/unit test/functional
+  ./node_modules/.bin/mongodb-test-runner -s -l -e single test/core test/unit test/functional
 else
   MONGODB_URI=${MONGODB_URI} npm run test
 fi
