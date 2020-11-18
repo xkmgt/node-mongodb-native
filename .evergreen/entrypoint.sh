6fe14068b252bd30f6bcc77fbc9051aff60593cd
emadum <eric.adum@mongodb.com>
2020-11-17 23:25:16 -0500

    test

--- .evergreen/entrypoint.sh
+++ .evergreen/entrypoint.sh
@@ -6,7 +6,9 @@ git clone --depth 1 -b v${DRIVER_VERSION} $DRIVER_REPOSITORY driver_src
 cd driver_src
 
 # Install desired version of Node
-nvm install --no-progress $NODE_VERSION && npm install
+export NVM_DIR=/root/.nvm
+$NVM_DIR/nvm.sh
+nvm install --no-progress $NODE_VERSION && npm install --silent
 
 # Run the tests with the provided MONGODB_URI
 if [ $DRIVER_VERSION == "3.6.0" ]; then
