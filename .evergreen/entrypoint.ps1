ee1f54b53f573ae70dd0acf2d5995f1a7be69202
emadum <eric.adum@mongodb.com>
2020-11-18 13:08:33 -0500

    updates

--- .evergreen/entrypoint.ps1
+++ .evergreen/entrypoint.ps1
@@ -1,4 +1,7 @@
 git clone --depth 1 -b v$env:DRIVER_VERSION $env:DRIVER_REPOSITORY driver_src
 cd driver_src
 npm install --silent
+# note: docker for windows doesn't support host networking
+$env:MONGODB_URI = $env:MONGODB_URI -replace "localhost", "host.docker.internal"
+[Environment]::SetEnvironmentVariable('PATH', $env:MONGODB_URI, [EnvironmentVariableTarget]::Machine); `
 npx mocha --recursive test/functional test/unit
